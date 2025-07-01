3df3a828b826d6447d31c50371a4ad048b333aaf
0xneves <asguilift@hotmail.com>
2025-07-01 12:01:09 -0300

    fix: last blocks grid

--- components/home/Block.vue
+++ components/home/Block.vue
@@ -17,7 +17,7 @@ const timeAgo = computed(() => {
 
 <template>
   <div class="flex items-center justify-between px-6 py-4 border-b border-gray-700">
-    <div class="flex items-center gap-4">
+    <div class="flex items-center w-1/3 gap-4">
       <div class="bg-[#151515] rounded-md p-3">
         <svg class="w-6 h-6 text-gray-400" width="800px" height="800px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" version="1.1" fill="none" stroke="#b0b0b0" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5">
           <polygon points="1.75 4.75 8 1.25 14.25 4.75 14.25 11.25 8 14.75 1.75 11.25"/>
@@ -32,19 +32,21 @@ const timeAgo = computed(() => {
       </div>
     </div>
 
-    <div class="text-sm">
-      <div class="text-[#f5f5f5]">
-        Chains {{ props.chainCount }}/20
-      </div>
-      <div>
-        <NuxtLink to="#" class="text-[#6ab5db] hover:text-[#9ccee7]">
-          {{ props.totalTransactions }} Transactions
-        </NuxtLink>
-      </div>
-    </div>
+    <div class="flex items-center justify-between w-2/3">
+        <div class="text-sm">
+          <div class="text-[#f5f5f5]">
+            Chains {{ props.chainCount }}/20
+          </div>
+          <div>
+            <NuxtLink to="#" class="text-[#6ab5db] hover:text-[#9ccee7]">
+              {{ props.totalTransactions }} Transactions
+            </NuxtLink>
+          </div>
+        </div>
 
-    <div class="text-xs text-gray-400 border border-gray-600 bg-gray-800 rounded-md px-2 py-1">
-      0.0 KDA
+      <div class="hidden sm:block text-xs text-gray-400 border border-gray-600 bg-gray-800 rounded-md px-2 py-1">
+        0.0 KDA
+      </div>
     </div>
   </div>
 </template>
