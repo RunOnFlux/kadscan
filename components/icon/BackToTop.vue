0a2144fefb4eec08467cca1f6a9aa10ce36faa48
0xneves <asguilift@hotmail.com>
2025-07-05 07:52:20 -0300

    feat: back to top on footer

---
+++ components/icon/BackToTop.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg width="24" height="24" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
+    <path d="M4 7.996h40v4H4zM34.828 24.996 24 14.168 13.171 24.996 16 27.824l6-6v18.172h4V21.824l6 6z"/>
+  </svg>
+</template> 
