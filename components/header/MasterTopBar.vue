a673f22282197279ddab3ebf285d26b6b7c417ac
0xneves <asguilift@hotmail.com>
2025-07-05 04:30:17 -0300

    feat: rem sticky

--- components/header/MasterTopBar.vue
+++ components/header/MasterTopBar.vue
@@ -1,19 +1,15 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const isSticky = ref(true);
 </script>
 
 <template>
   <div>
     <div
-      class="w-full h-[44px] z-50 bg-[#111111] border-b border-[#222222]"
-      :class="{
-        'fixed top-0 left-0': isSticky,
-      }"
+      class="w-full h-[44px] z-50 bg-[#111111] border-b border-[#222222] fixed top-0 left-0"
     >
       <!-- This is the new, empty MasterTopBar -->
     </div>
-    <div v-if="isSticky" class="h-[44px]"></div>
+    <div class="h-[44px]"></div>
   </div>
 </template> 
